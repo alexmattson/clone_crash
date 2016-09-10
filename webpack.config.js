cad37fb12b313018266669f9187a800cea3e564d
amattson21 <amattson21@gmail.com>
2016-09-10 15:24:58 -0700

    ship flying around

--- webpack.config.js
+++ webpack.config.js
@@ -8,21 +8,5 @@ module.exports = {
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel',
-        query: {
-          presets: ['react', 'es2015']
-        }
-      },
-      {
-        test: /\.node$/,
-        loader: 'node-loader'
-      }
-    ]
-  },
   devtool: 'source-maps'
 };
